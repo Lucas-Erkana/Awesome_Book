edfe7669d06dfcc9e09fb9b735f6506a57de7d6d
unknown <kashif_saleem_23@hotmail.com>
2022-11-30 14:11:46 +0500

    update main.js

--- main.js
+++ main.js
@@ -1,73 +1,91 @@
 /* eslint-disable linebreak-style */
 import Book from './module/constructor.js';
 import Store from './module/store.js';
-// UI Class: Handle UI Tasks
-class UI {
-  static displayBooks() {
-    const books = Store.getBooks();
+import Navigation from './module/navigation.js';
+import Layout from './module/layout.js';
 
-    books.forEach((book) => UI.addBookToList(book));
+const date = window.luxon;
+
+export default class UI {
+  static displayBook() {
+    const bookList = Store.getBooks();
+    bookList.forEach((book) => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
-    const list = document.querySelector('#book-list');
-    const row = document.createElement('tr');
-    // element.classList.add('table-row');
-    row.innerHTML = `
-      <td class="book-title">"${book.title}" </td>
-      <td> by</td>
-      <td>${book.author}</td>
-      <td>
-      <input class='btn btn-danger btn-sm delete' type="button" value="Remove">
-     </td>
-    `;
-    list.appendChild(row);
+    const listOutput = document.querySelector('.listDisplay');
+    listOutput.innerHTML += `<div class="book rounded">
+                               <div class="book-infos py-2" id="${book.id}">   
+                                 <span class="px-2 fw-bolder item1 blck text-capitalize">"${book.title}"</span>
+                                 <span id="by" class="px-2">by</span>
+                                 <span id="spanAuthor" class="px-2 item3 blck text-capitalize"><span id="by1">by:</span> ${book.author}</span>
+                                 <button id="removeBtn" class="position-absolute h5 py-1 px-4 removeBtn clickable rounded end-0 bottom-0" >Delete</button>
+                               </div>
+                             </div>
+                            `;
   }
 
-  // delete function
-  static deleteBook(el) {
-    if (el.classList.contains('delete')) {
-      el.parentElement.parentElement.remove();
-    }
-  }
-
-  // Create clear fields function
-  static clearFields() {
-    document.querySelector('#title').value = '';
-    document.querySelector('#author').value = '';
+  static deleteBook(id) {
+    const el = document.getElementById(`${id}`);
+    el.parentNode.removeChild(el);
   }
 }
 
-// Event: Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+function addedSuccess() {
+  document.getElementById('addMsg').style = 'display:block';
+  document.getElementById('addMsg').innerHTML = 'Book added successfully!!!';
+  setTimeout(() => {
+    document.getElementById('addMsg').style = 'display:none';
+  }, 2000);
+}
 
-// Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  // Prevent actual submit
+document.addEventListener('submit', (e) => {
   e.preventDefault();
-
-  // Get form values
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-
-  // Instatiate book
-  const book = new Book(title, author);
-
-  // Add Book to UI
-  UI.addBookToList(book);
-
-  // Add book to storage
-  Store.addBook(book);
-
-  // Clear fields
-  UI.clearFields();
+  // if (e.target.title.value === '' || e.target.author.value === '') {
+  //   alert('Input field cannot be empty');
+  // // } else {
+  const author = e.target.author.value.trim();
+  const title = e.target.title.value.trim();
+  const newBook = new Book(title, author);
+  Store.addBook(newBook);
+  UI.addBookToList(newBook);
+  e.target.reset();
+  document.querySelectorAll('.content').forEach((item) => {
+    item.classList.remove('active');
+  });
+  const content = document.querySelector('#form');
+  content.classList.add('active');
+  addedSuccess();
+  // }
 });
 
-// Delete a book
-document.querySelector('#book-list').addEventListener('click', (e) => {
-  // remove book from UI
-  UI.deleteBook(e.target);
+const layout = new Layout();
+layout.update();
 
-  // remove book from broswer storage according to author
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+document.querySelector('.listDisplay').addEventListener('click', (e) => {
+  const isButton = e.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+  UI.deleteBook(e.target.parentElement.id);
+  Store.removeBook(e.target.parentElement.id);
+});
+
+// Imlplement responsive menu
+
+const menu = document.querySelector('.header');
+
+document.getElementById('humburger').onclick = () => {
+  menu.classList.add('change-menu');
+  // alert('hello')
+};
+
+document.getElementById('closeIcon').addEventListener('click', () => {
+  menu.classList.remove('change-menu');
+});
+
+window.addEventListener('mouseup', (e) => {
+  if (e.target !== document.getElementById('closeIcon') && e.target !== document.getElementById('humburger')) {
+    menu.classList.remove('change-menu');
+  }
 });
--- module/layout.js
+++ module/layout.js
@@ -1,16 +1,16 @@
 /* eslint-disable linebreak-style */
-import UI from "../main.js";
-import Navigation from "./navigation.js";
+import UI from '../main.js';
+import Navigation from './navigation.js';
 
 const date = window.luxon;
 export default class Layout {
-    constructor() {
-        this.container = document.getElementById('container');
-        this.container.classList.add('layout');
-        this.header = document.createElement('header');
-        this.header.setAttribute('id', 'header');
-        this.header.classList.add('header', 'position-fixed', 'top-0', 'w-100');
-        this.header.innerHTML = `<nav class="navbar px-3">
+  constructor() {
+    this.container = document.getElementById('container');
+    this.container.classList.add('layout');
+    this.header = document.createElement('header');
+    this.header.setAttribute('id', 'header');
+    this.header.classList.add('header', 'position-fixed', 'top-0', 'w-100');
+    this.header.innerHTML = `<nav class="navbar px-3">
                                  <div class="page-title">
                                    <a href="#list" class="page-title text-white active">Awesome Books</a>
                                  </div>
@@ -29,37 +29,37 @@ export default class Layout {
                                  </ul>
                                </nav>`;
 
-        const today = date.DateTime.local();
-        this.time = document.createElement('div');
-        this.time.classList.add('clock', 'mt-5');
-        this.time.innerHTML = `<p id="clock-parag" class="pt-4 text-end me-2">
+    const today = date.DateTime.local();
+    this.time = document.createElement('div');
+    this.time.classList.add('clock', 'mt-5');
+    this.time.innerHTML = `<p id="clock-parag" class="pt-4 text-end me-2">
                                ${today.toFormat('FFF')}
                              </p>
                              `;
-        this.main = document.createElement('main');
-        this.main.classList.add('main-container');
-        this.bookList = document.createElement('div');
-        this.bookList.id = 'list';
-        this.bookList.classList.add(
-            'content',
-            'book-list',
-            'active',
-            'content-box',
-            'pt-2',
-        );
-        this.bookList.innerHTML = `<div class="list-title">
+    this.main = document.createElement('main');
+    this.main.classList.add('main-container');
+    this.bookList = document.createElement('div');
+    this.bookList.id = 'list';
+    this.bookList.classList.add(
+      'content',
+      'book-list',
+      'active',
+      'content-box',
+      'pt-2',
+    );
+    this.bookList.innerHTML = `<div class="list-title">
                                     <h3 class="fw-bold fs-4 pb-4 flex-center-column">All awesome books</h3>
                                   </div>
                                   <div id="listDisplay" class="listDisplay list-output w-100 rounded"></div>`;
-        this.NewBookForm = document.createElement('form');
-        this.NewBookForm.id = 'form';
-        this.NewBookForm.classList.add(
-            'content',
-            'form',
-            'content-box',
-            'w-100',
-        );
-        this.NewBookForm.innerHTML = `<div class="form-title flex-center-column pt-3">
+    this.NewBookForm = document.createElement('form');
+    this.NewBookForm.id = 'form';
+    this.NewBookForm.classList.add(
+      'content',
+      'form',
+      'content-box',
+      'w-100',
+    );
+    this.NewBookForm.innerHTML = `<div class="form-title flex-center-column pt-3">
                                       <h3 class="fw-bold fs-4 w-100 pt-2 pb-2 flex-center-column">Add a new book</h3>
                                     </div>
                                     <p id="addMsg" class="text-center text-success"></p>
@@ -73,17 +73,17 @@ export default class Layout {
                                       <input type="submit" class="btn btn btn-dark px-5 clickable mt-0" value="Add Book" id="insertBtn">
                                     </div>
                                     `;
-        this.contactInfos = document.createElement('div');
-        this.contactInfos.classList.add(
-            'w-100',
-            'text-center',
-            'content-box',
-            'content',
-            'pt-2',
-        );
-        this.contactInfos.setAttribute('id', 'contact');
-        this.contactInfos.classList.add('contact');
-        this.contactInfos.innerHTML = `<div class="flex-center-column contactBox w-75 p-1">
+    this.contactInfos = document.createElement('div');
+    this.contactInfos.classList.add(
+      'w-100',
+      'text-center',
+      'content-box',
+      'content',
+      'pt-2',
+    );
+    this.contactInfos.setAttribute('id', 'contact');
+    this.contactInfos.classList.add('contact');
+    this.contactInfos.innerHTML = `<div class="flex-center-column contactBox w-75 p-1">
                                        <div class="pb-2 w-100 contact-title flex-center-column">
                                          <h3 class="fw-bold fs-4 w-100 flex-center-column position-relative">Contact informarion</h3>
                                        </div>
@@ -95,24 +95,24 @@ export default class Layout {
                                          <li class="list-group-item">Our address:<span class="fw-bold"> streetname 22, 84503 city, country</span></li>
                                        </ul>
                                      </div>`;
-        this.footer = document.createElement('footer');
-        this.footer.setAttribute('id', 'footer');
-        this.footer.classList.add(
-            'footer',
-            'position-fixed',
-            'bottom-0',
-            'w-100',
-        );
-        this.footer.innerHTML = `<div class="copyright py-3 text-start me-3">
+    this.footer = document.createElement('footer');
+    this.footer.setAttribute('id', 'footer');
+    this.footer.classList.add(
+      'footer',
+      'position-fixed',
+      'bottom-0',
+      'w-100',
+    );
+    this.footer.innerHTML = `<div class="copyright py-3 text-start me-3">
                                  <p class="h6 text-white">Copyright </p>
                                </div>`;
-        this.container.append(this.header, this.time, this.main, this.footer);
-        this.main.append(this.bookList, this.NewBookForm, this.contactInfos);
-    }
+    this.container.append(this.header, this.time, this.main, this.footer);
+    this.main.append(this.bookList, this.NewBookForm, this.contactInfos);
+  }
 
-    update() {
-        UI.displayBook();
-        const nav = new Navigation();
-        nav.init();
-    }
+  update() {
+    UI.displayBook();
+    const nav = new Navigation();
+    nav.init();
+  }
 }
